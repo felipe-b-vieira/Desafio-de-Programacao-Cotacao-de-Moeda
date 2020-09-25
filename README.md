d8fd93a1aeb78a0232957186790e45e25541bb37
Felipe Vieira <felipe_brito@id.uff.br>
2020-09-25 02:22:13 -0300

    Ajuste de web scrapping para listagem de países em Português
    Também foi gerado os valores de saída que serão usados como resposta do programa.

--- README.md
+++ README.md
@@ -4,10 +4,14 @@
 # Solução do Problema
  O código apresentado utiliza o arquivo csv disponibilizado pelo Banco Central. O arquivo csv que é usado pode ser acessado pelo link https://www.bcb.gov.br/estabilidadefinanceira/cotacoestodas.
  Através desse arquivo, conseguimos pegar os valores de dólar de cada moeda.
+ 
+ Para execução do código, basta executar o arquivo menor_cotacao_data_especifica.py com python3. Insira a data e um csv será pego, se possível, do site. Um dataframe vai ser gerado com as informações da moeda para o cálculo do dolar, e por fim, será encontrado o menor valor do dólar e retornado as informações desejadas.
+ Para descobrir o nome do país, é feito um web scrapping na página de link https://pt.wikipedia.org/wiki/ISO_4217 da Wikipedia, retornando o código da Moeda e seu nome. Através dessa ligação é possível gerar os nomes dos países que usam cada moeda.
 
 #Bibliotecas necessárias:
  - requests
  - pandas
+ - BeautifulSoup
  
 # Problema apresentado:
 
--- menor_cotacao_data_especifica.py
+++ menor_cotacao_data_especifica.py
@@ -1,37 +1,116 @@
 import requests
 import pandas as pd
+from bs4 import BeautifulSoup
+
+#CONSTANTES
+MONTANTE = 1
 
 
 #Chama a api para pegar a listagem de todas as moedas com suas cotacoes
 def gera_dataframe_moedas_cotacao(dia,mes,ano):
 	#faz requisição para a API do Banco Central através de um endereço que retorna um arquivo com todas as cotações(Sem BRL e USD)
     #Usando valor de endereço de teste e pegando seu csv e adicionando em um dataframe de pandas
-    df_csv_moedas=pd.read_csv("https://www4.bcb.gov.br/Download/fechamento/20200923.csv",sep=';',header=None,names=["#","Cod Moeda","Tipo","Moeda","Taxa Compra","Taxa Venda","Paridade Compra","Paridade Venda"])
+    #O Csv não tem header, preciso adicionar manualmente(Iguais ao site)
+    df_csv_moedas=pd.read_csv("https://www4.bcb.gov.br/Download/fechamento/20190923.csv",sep=';',header=None,names=["#","Cod Moeda","Tipo","Moeda","Taxa Compra","Taxa Venda","Paridade Compra","Paridade Venda"])
     return df_csv_moedas
 
 
-def calcular_dolar_df(df):
+def calcular_dolar_df(df_moedas,MONTANTE):
     #itera sobre cada linha do Dataframe para calcular o valor do dólar com base no cálculo passado pelo site(Depende do tipo de Moeda)
     valores_dolar=[]
-    for index, row in df.iterrows():
-        if(row["Tipo"]=="A"):
-            valores_dolar.append(1/float(row["Paridade Compra"].replace(',','.')))
-        elif(row["Tipo"]=="B"):
-            valores_dolar.append(1*float(row["Paridade Compra"].replace(',','.')))
+    for indice, infos_moeda in df_moedas.iterrows():
+        #Usa paridade de compra para mostrar o valor de dólar da moeda
+        paridade_moeda = float(infos_moeda["Paridade Compra"].replace(',','.'))
+        
+        if(infos_moeda["Tipo"]=="A"): 
+            #Divide pela paridade
+            valores_dolar.append(MONTANTE/paridade_moeda)
+        elif(infos_moeda["Tipo"]=="B"): 
+            #Multiplica pela paridade
+            valores_dolar.append(MONTANTE*paridade_moeda)
         else:
-            valores_dolar.append(-1)
-    df["Valor_dolar"] = valores_dolar
-    return df
-
-
-
-def acha_menor_valor_dolar(df):
+            #Valor arbitrário negativo para o caso de ter um tipo diferente
+            valores_dolar.append(-MONTANTE)
     
-    #acha o menor valor de dolar na linha de valores do dólar
-    #pos_valores_minimos_df = df.idxmin()
-    print(df["Valor_dolar"])
+    df_moedas["Valor_dolar"] = valores_dolar
+    return df_moedas
+    
+    
+#Para mantar o trabalho todo em português, foi necessário realizar web scrapping para pegar os nomes dos países brasileiros com base no seu código
+def gera_listagem_nomes_pais():
+    #Utiliza o link da Wikipedia que lista os países que usam cada moeda
+    URL_codigos = "https://pt.wikipedia.org/wiki/ISO_4217"
+    resposta_wikipedia = requests.get(URL_codigos)
+
+    #transforma em soup com parser html do beautifulsoup e recupero a tabela com os nomes do país
+    soup_listagem_paises = BeautifulSoup(resposta_wikipedia.content, 'html.parser')
+    tabela_paises = soup_listagem_paises.find('table',{"class":"wikitable sortable"})
+    
+    dict_paises = {}
+    #pego cada linha da tabela
+    for linha_paises in tabela_paises.findChildren(['tr']):
+        #pego as celulas de cada linha, a primeira célula é o código do país. A quinta célula é a listagem de países que usam essa moeda
+        celulas_paises = linha_paises.findChildren(['td'])
+        #verifico se tem o tamanho correto e é um valor válido para salvar
+        if(len(celulas_paises)>=4):
+            #pego o código(Primeira célula)
+            codigo_pais = celulas_paises[0].string
+            #verifico todos os links na quinta célular para encontrar o nome de todos os países
+            nomes_paises = []
+            ahref_nomes_paises = celulas_paises[4].findAll(['a'])
+            for nomep in ahref_nomes_paises:
+                nomes_paises.append(nomep.string)
+            #salvo o código como chave de um dicionário com seu valor sendo um array dos paises
+            dict_paises[codigo_pais] = nomes_paises
+    
+    return dict_paises    
+    
+    
+    '''
+    #====Código utilizado para Web Scrapping em outra página========
+    #Pegamos os nomes e códigos separadamente, mas ambos possuem a mesma quantidade no site
+    #pega todos os links na tabela de paises, seus títulos são os nomes dos países
+    nomes_paises=[]
+    for nomes_paises_ahref in tabela_paises.findChildren(['a']):
+        nomes_paises.append(nomes_paises_ahref.get('title'))
+    
+    #pega todas linhas da tabela, elas contêm o código
+    codigos_paises=[]
+    linhas_paises = tabela_paises.findChildren(['tr'])
+    for codigo_pais_atual in codigos_e_paises:
+        #pega as células da linha, o primeiro valor é a célula com o código
+        celulas_paises = codigo_pais_atual.findChildren('td')
+        codigo_pais=val_codigo_pais[0].string
+        #salva no vetor de código
+        if(codigo_pais!=None):
+            codigos_paises.append(codigo_pais)
+            
+    #retorna um dicionario com os códigos como chave e nomes dos países como valores
+    return dict(zip(codigos_paises, nomes_paises))
+    '''
 
 
-df_moedas_cotacao = gera_dataframe_moedas_cotacao(1,1,1)
-df_moedas_cotacao = calcular_dolar_df(df_moedas_cotacao)
-acha_menor_valor_dolar(df_moedas_cotacao)
+
+def acha_infos_menor_dolar(df_moedas):    
+    #pega a linha do dataframe de moedas que tenha o menor valor do dólar
+    menor_moeda = df_moedas[df_moedas["Valor_dolar"]==df_moedas["Valor_dolar"].min()]
+    
+    #pega os valores que queremos, usa iloc para pegar o único valor e seu conteúdo
+    simbolo_moeda = menor_moeda["Moeda"].iloc(0)[0]
+    cotacao_dolar = menor_moeda["Valor_dolar"].iloc(0)[0]
+    #Utiliza web scrapping para pegar listagem de nome do país em português
+    dict_paises = gera_listagem_nomes_pais()
+    #Uso o dicionario dos países para pegar a listagem de países dado o símbolo da moeda
+    pais_moeda = dict_paises[simbolo_moeda]
+    
+    print(pais_moeda)
+    
+    
+
+    
+
+
+if __name__ == "__main__":
+    df_moedas_cotacao = gera_dataframe_moedas_cotacao(1,1,1)
+    df_moedas_cotacao = calcular_dolar_df(df_moedas_cotacao,MONTANTE)
+    acha_infos_menor_dolar(df_moedas_cotacao)
